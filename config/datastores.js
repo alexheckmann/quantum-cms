9f5802545f03c385ba58cccc954cd261985e5d85
Alex <alex.aichholz@gmail.com>
2021-11-23 22:46:47 +0100

    Database connection made

--- config/datastores.js
+++ config/datastores.js
@@ -50,7 +50,12 @@ module.exports.datastores = {
     ***************************************************************************/
     // adapter: 'sails-mysql',
     // url: 'mysql://user:password@host:port/database',
-
+    adapter: 'sails-mysql',
+    host: '193.196.53.194',
+    port: 33061,
+    user: 'user3',
+    password: 'n115113',
+    database: 'qntmcms'
   },
 
 
