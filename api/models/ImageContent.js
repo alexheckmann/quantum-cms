70f502daf61cb1eb863ffd1891d04081e96a82bd
Alex <alex.aichholz@gmail.com>
2021-11-30 09:14:17 +0100

    model relationships

--- api/models/ImageContent.js
+++ api/models/ImageContent.js
@@ -1,9 +1,18 @@
 module.exports = {
     attributes: {
-        version: { type: 'string', columnType: 'varchar(80)'/* TODO zusammengesetzter PK */},
         status: { type: 'string', columnType: 'varchar(80)' },
         title: { type: 'string', columnType: 'varchar(80)' },
         content: { type: 'string', columnType: 'varchar(80)' /* TODO spezieller Datentyp */},
         endpoint: { type: 'string', columnType: 'varchar(80)' },
+
+        tags: {
+            collection: 'tag',
+            via: 'imageContentTag'
+        },
+
+        oldversions: {
+            collection: 'imagecontentarchive',
+            via: 'newestVersion'
+          },
     },
 };
--- api/models/TextContent.js
+++ api/models/TextContent.js
@@ -4,9 +4,24 @@ module.exports = {
     content: {type: 'string', columnType: 'varchar(1024)'},
     endpoint: {type: 'string', columnType: 'varchar(80)'},
     status: {type: 'string', columnType: 'varchar(80)'},
+
     oldversions: {
       collection: 'textcontentarchive',
       via: 'newestVersion'
+    },
+
+    author: {
+      model: 'user'
+    },
+
+    updatedFrom: {
+      model: 'user'
+    },
+
+    tags: {
+      collection: 'tag',
+      via: 'textContentTag'
     }
+
   },
 };
