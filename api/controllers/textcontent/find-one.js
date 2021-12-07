3334c2a45f405f877371671226a2d1df6024b880
Alex <alex.aichholz@gmail.com>
2021-12-07 13:21:41 +0100

    controllers improved

--- api/controllers/textcontent/find-one.js
+++ api/controllers/textcontent/find-one.js
@@ -1,39 +1,39 @@
 module.exports = {
 
 
-    friendlyName: 'Find one',
-  
-  
-    description: '',
-  
-  
-    inputs: {
-      id: {
-        description: 'The text content of the user to look up.',
-        type: 'number',
-        required: true
-      }
-    },
-  
-  
-    exits: {
-      success: {
-        responseType: 'view',
-        viewTemplatePath: 'pages/textcontent/show'
-      },
-      notFound: {
-        description: 'No text content with the specified ID was found in the database.',
-        responseType: 'notFound'
-      }
-    },
-  
-    fn: async function ({id}) {
-  
-      let textcontent = await TextContent.findOne({ id: id });
-      if (!textcontent) { throw 'notFound'; }
-      return {
-        message : "",
-        textcontent: textcontent
-      };
+  friendlyName: 'Find one',
+
+
+  description: '',
+
+
+  inputs: {
+    id: {
+      description: 'The text content of the user to look up.',
+      type: 'number',
+      required: true
     }
-  };
+  },
+
+
+  exits: {
+    success: {
+      responseType: 'view',
+      viewTemplatePath: 'pages/textcontent/show'
+    },
+    notFound: {
+      description: 'No text content with the specified ID was found in the database.',
+      responseType: 'notFound'
+    }
+  },
+
+  fn: async function ({ id }) {
+
+    let textcontent = await TextContent.findOne({ id: id });
+    if (!textcontent) { throw 'notFound'; }
+    return {
+      message: "",
+      textcontent: textcontent
+    };
+  }
+};
--- api/controllers/textcontent/find.js
+++ api/controllers/textcontent/find.js
@@ -1,37 +1,39 @@
 module.exports = {
 
-    friendlyName: 'Find',
-  
-    description: 'Find text content.',
-  
-    inputs: {
-      q: {
-        description: 'The search input.',
-        type: 'string',
-        required: false
-      }
-    },
-  
-    exits: {
-      success: {
-        responseType: 'view',
-        viewTemplatePath: 'pages/textcontent/index'
-      }
-    },
-  
-    fn: async function (inputs) {
-      let textcontents;
-      if (inputs.q && inputs.q.length > 0) {
-        textcontents = await TextContent.find({
-          title: {
-            'contains': inputs.q
-          }
-        })
-      } else {
-        textcontents = await TextContent.find();
-      }
-      sails.log.debug(textcontents)
-      return ({ textcontents: textcontents });
+  friendlyName: 'Find',
+
+  description: 'Find text content.',
+
+  inputs: {
+    q: {
+      description: 'The search input.',
+      type: 'string',
+      required: false
     }
-  };
-  
+  },
+
+  exits: {
+    success: {
+      responseType: 'view',
+      viewTemplatePath: 'pages/textcontent/index'
+    }
+  },
+
+  fn: async function (inputs) {
+    let textcontents;
+    if (inputs.q && inputs.q.length > 0) {
+      textcontents = await TextContent.find({
+        title: {
+          'contains': inputs.q
+        },
+        author: this.req.session.userId
+      })
+    } else {
+      textcontents = await TextContent.find({
+        author: this.req.session.userId
+      });
+    }
+    sails.log.debug(textcontents)
+    return ({ textcontents: textcontents });
+  }
+};
