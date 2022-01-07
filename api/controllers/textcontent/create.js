1acbd2d29199873bff6b5b05170846d55141fe41
alexheckmann <ah@ecommunications.de>
2022-01-07 21:19:14 +0100

    added AJAX for editor

--- api/controllers/textcontent/create.js
+++ api/controllers/textcontent/create.js
@@ -1,58 +1,36 @@
 module.exports = {
 
 
-    friendlyName: 'Createtextcontent',
+  friendlyName: 'Createtextcontent',
 
 
-    description: 'Create text content.',
+  description: 'Create text content.',
 
 
-    inputs: {
-      title: {
-        description: 'The description of the content.',
-        type: 'string',
-        maxLength: 80,
-        required: true
-      },
-      content: {
-        description: 'The text content.',
-        type: 'string',
-        maxLength: 1024,
-      },
+  inputs: {
+
+  },
+
+
+  exits: {
+    success: {
+      responseType: 'view',
+      viewTemplatePath: 'pages/textcontent/show'
     },
+  },
 
 
-    exits: {
-      success: {
-        responseType: 'view',
-        viewTemplatePath: 'pages/textcontent/show'
-      },
-    },
+  fn: async function (inputs) {
+    // load navbar items
+    let textcontents = await TextContent.find({
+      author: this.req.session.userId
+    }).sort('title ASC');
 
 
-    fn: async function (inputs) {
-      // load navbar items
-      let textcontents = await TextContent.find({
-        author: this.req.session.userId
-      }).sort('title ASC');
+    return {
+      message: 'Successfully created.',
+      textcontents: textcontents,
+    };
+  }
 
-      let textcontent = {
-        author: this.req.session.userId,
-        status: 'active',
-        endpoint: 'https://qntm-cms.herokuapp.com/textcontent/api/'+inputs.id,
-        title: inputs.title,
-        content: inputs.content
-      };
-
-      textcontent = await TextContent.create(textcontent).fetch();
-
-      sails.log.debug(textcontent)
-      if (!textcontent) { throw 'notFound'; }
-      return {
-        message: "Successfully created.",
-        textcontents: textcontents,
-        textcontent: textcontent
-      };
-    }
-
-  };
+};
