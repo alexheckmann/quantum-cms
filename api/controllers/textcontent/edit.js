eacad967844c98e47252ae4f4dc658911810f052
alexheckmann <ah@ecommunications.de>
2022-01-15 21:25:56 +0100

    moved Date conversion logic to the clientside

--- api/controllers/textcontent/edit.js
+++ api/controllers/textcontent/edit.js
@@ -40,8 +40,6 @@ module.exports = {
     // load individual record
     let textcontent = await TextContent.findOne({ id: id }).populate('author').populate('updatedFrom').populate('tags').populate('oldversions');
     if (!textcontent) { throw 'notFound'; }
-    textcontent.createdAt = convertDate(textcontent.createdAt);
-    textcontent.updatedAt = convertDate(textcontent.updatedAt);
     return {
       textcontents: textcontents,
       textcontent: textcontent
@@ -49,23 +47,4 @@ module.exports = {
   }
 };
 
-/* Converts a JS timestamp from milliseconds to readable time format */
 
-/* TODO move to own file? */
-function convertDate(date) {
-  let ISODate = new Date(date);
-
-  let day = ISODate.getDate();
-  let month = ISODate.getMonth() + 1;
-  let year = ISODate.getFullYear();
-  let hour = ISODate.getHours();
-  let min = ISODate.getMinutes();
-
-  // adds leading zero
-  month = (month < 10 ? '0' : '') + month;
-  day = (day < 10 ? '0' : '') + day;
-  hour = (hour < 10 ? '0' : '') + hour;
-  min = (min < 10 ? '0' : '') + min;
-
-  return day + '/' + month + '/' + year + ' ' + hour + ':' + min;
-}
