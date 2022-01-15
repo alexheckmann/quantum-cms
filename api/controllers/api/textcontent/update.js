8f93ab9b4275fffd6c82f74d659752cdc4a79f6e
alexheckmann <ah@ecommunications.de>
2022-01-15 21:28:54 +0100

    content now saved via AJAX calls

---
+++ api/controllers/api/textcontent/update.js
@@ -0,0 +1,76 @@
+module.exports = {
+
+
+  friendlyName: 'Update',
+
+
+  description: 'Update text content.',
+
+
+  inputs: {
+    id: {
+      description: 'The id of the text content.',
+      type: 'number',
+      required: true
+    },
+    title: {
+      description: 'The description of the content.',
+      type: 'string',
+      maxLength: 80,
+      required: true
+    },
+    content: {
+      description: 'The text content.',
+      type: 'string',
+      maxLength: 1024
+    },
+  },
+
+
+  exits: {},
+
+
+  fn: async function (inputs) {
+    // fetch the TextContent for archiving from the DB
+    let oldTextContent = await TextContent.findOne({id: inputs.id});
+    if (!oldTextContent) {
+      throw 'notFound TextContent: ' + inputs.id;
+    } else {
+      // write TextContent to archive
+      let textcontentarchive = {
+        title: oldTextContent.title,
+        content: oldTextContent.content,
+        newestVersion: oldTextContent.id,
+      };
+      textcontentarchive = await TextContentArchive.create(textcontentarchive).fetch();
+      sails.log.debug('Archive textcontent:');
+      sails.log.debug(textcontentarchive);
+    }
+
+    // update TextContent
+    let textcontent = {
+      title: inputs.title,
+      content: inputs.content,
+      updatedFrom: this.req.session.userId,
+      endpoint: 'https://qntm-cms.herokuapp.com/textcontent/' + inputs.id
+    };
+    textcontent = await TextContent.updateOne({id: inputs.id}).set(textcontent);
+    sails.log.debug('Updated textcontent:');
+    sails.log.debug(textcontent);
+
+    if (!textcontent) {
+      throw 'notFound TextContent with ID: ' + textcontent.id;
+    }
+    let author = await User.findOne({
+      where: {
+        id: textcontent.updatedFrom
+      },
+      select: ['fullName']
+    });
+    return {
+      updatedAt: textcontent.updatedAt,
+      updatedFrom: author.fullName
+    };
+  }
+
+};
