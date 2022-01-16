22946185ae125f5cca231e7c08ac279c741231f2
alexheckmann <ah@ecommunications.de>
2022-01-16 10:00:24 +0100

    removed old code that was replaced by ajax

--- api/controllers/api/textcontent/destroy.js
+++ api/controllers/api/textcontent/destroy.js
@@ -22,12 +22,6 @@ module.exports = {
 
 
   fn: async function ({ id }) {
-    sails.log.debug('Destroy textcontent: ' + id);
     let textcontent = await TextContent.destroyOne({ id: id });
-    if (textcontent) {
-      sails.log('Destroyed textcontent: ' + textcontent.id);
-    } else {
-      sails.log('Can not destroy textcontent: ' + textcontent.id);
-    }
   }
 };
