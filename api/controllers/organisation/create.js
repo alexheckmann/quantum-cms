8780b03dca76c57986a605d191b8aea807bb9302
alexheckmann <ah@ecommunications.de>
2022-01-23 17:44:06 +0100

    added random hex generator

--- api/controllers/organisation/create.js
+++ api/controllers/organisation/create.js
@@ -25,16 +25,22 @@ module.exports = {
 
 
   fn: async function (inputs) {
+
+    const randomHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+
     // create new organisation
     let org = {
       name: inputs.organisation,
+      inviteCode: randomHex(8)
     };
+
+
     org = await Organisation.create(org).fetch();
     sails.log.debug('Created Organisation:');
     sails.log.debug(org);
 
     // adds the user who created the organisation to it and gives him admin rights.
-    let user = await User.updateOne({id: this.req.me.id }).set({
+    let user = await User.updateOne({id: this.req.me.id}).set({
       organisation: org.id,
       admin: org.id
     });
@@ -42,7 +48,9 @@ module.exports = {
     sails.log.debug('User:');
     sails.log.debug(user);
 
-    if (!org) { throw 'notFound'; }
+    if (!org) {
+      throw 'notFound';
+    }
     return {
       message: 'Organisation successfully created.',
       org: org
