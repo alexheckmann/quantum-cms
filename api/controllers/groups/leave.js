021b0fe7ac6f54a57492c965e1b7f0bfc7e54439
alexheckmann <ah@ecommunications.de>
2022-01-25 10:37:25 +0100

    fixed a bug that made leaving groups impossible if entries exist

--- api/controllers/groups/leave.js
+++ api/controllers/groups/leave.js
@@ -29,6 +29,7 @@ module.exports = {
     let grp = await WorkingGroup.findOne({ id: inputs.id }).populate('workers').populate('admins');
 
     if ((grp.workers.length === 1 && grp.admins.length === 0) || (grp.workers.length === 1 && grp.admins.length === 1)) {
+      await TextContent.destroy({group: grp.id});
       await WorkingGroup.destroyOne({id: grp.id});
     } else {
       await User.removeFromCollection(this.req.me.id, 'workingGroups', inputs.id);
